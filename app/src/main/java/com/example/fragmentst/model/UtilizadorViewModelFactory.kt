9595846b2cec22e5b97b3924733696c60af3814b
JesseSacramento01 <21938@stu.ipbeja.pt>
2024-10-22 12:30:04 +0100

    UtilizadorViewModel Added

---
+++ app/src/main/java/com/example/fragmentst/model/UtilizadorViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.example.fragmentst.model
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+import com.example.fragmentst.repository.UtilizadorRepository
+
+class UtilizadorViewModelFactory (
+    private val application: Application,
+    private val repository: UtilizadorRepository
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(UtilizadorViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return UtilizadorViewModel(application, repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
